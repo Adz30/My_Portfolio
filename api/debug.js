b3e86edfa865055a223c5916c09aa37c43c855ed
adam <kaw94@hotmail.co.uk>
2025-07-04 12:44:04 +0100

    api debug

--- api/debug.js
+++ api/debug.js
@@ -1,7 +1,16 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    NOTION_API_KEY: process.env.NOTION_API_KEY,
-    isDefined: !!process.env.NOTION_API_KEY,
-    length: process.env.NOTION_API_KEY?.length || 0,
-  });
+import { Client } from "@notionhq/client";
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+async function test() {
+  try {
+    const response = await notion.databases.retrieve({
+      database_id: process.env.NOTION_DATABASE_ID,
+    });
+    console.log("Database retrieved:", response.title[0].plain_text);
+  } catch (e) {
+    console.error("Error:", e);
+  }
 }
+
+test();
